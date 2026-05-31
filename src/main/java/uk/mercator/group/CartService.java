5912d185b4f34615d176f7a7e1ee3998907bc98a
Dibyatanu Deb <dibyatanu.deb1@yahoo.com>
2026-05-31 11:09:20 +0100

    Step2- buy one get one apple for free

--- src/main/java/uk/mercator/group/CartService.java
+++ src/main/java/uk/mercator/group/CartService.java
@@ -4,6 +4,8 @@ package uk.mercator.group;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public final class CartService {
     final private Map<Fruits,Integer> fruitBasket = new HashMap<>();
@@ -24,14 +26,24 @@ public final class CartService {
         double total = 0;
         total += fruitBasket.entrySet()
                 .stream()
+                .map(applyOffers)
                 .mapToInt(entry -> entry.getKey().getUnitCost() * entry.getValue())
                 .sum();
         return total;
     }
 
-    public static String formatCurrency(double value) {
+    private final Function<Map.Entry<Fruits, Integer>, Map.Entry<Fruits, Integer>> applyOffers =
+            entry -> {
+                if (entry.getKey() == Fruits.APPLE) {
+                    entry.setValue((entry.getValue() + 1) / 2);
+                }
+                return entry;
+            };
+
+    private String formatCurrency(double value) {
         return (value >= 1)
                 ? String.format("£%.2f", value)
                 : String.format("%.2fp",value);
     }
+
 }
